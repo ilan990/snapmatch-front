0c0f22c73c43b8f09142d70c5c9516e22be70c45
ilan990 <77548546+ilan990@users.noreply.github.com>
2024-06-20 14:52:22 +0200

    Installation de tailwindCss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css'; 
+import 'tailwindcss/tailwind.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">Application de Gestion de Photos</h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-white p-4 shadow-md rounded-lg">
+          {/* Contenu du premier composant */}
+        </div>
+        <div className="bg-white p-4 shadow-md rounded-lg">
+          {/* Contenu du deuxième composant */}
+        </div>
+        <div className="bg-white p-4 shadow-md rounded-lg">
+          {/* Contenu du troisième composant */}
+        </div>
+      </div>
     </div>
   );
 }
